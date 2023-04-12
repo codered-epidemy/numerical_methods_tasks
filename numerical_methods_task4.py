19d326b009c48f6e84e4b39f393c0a1148e588a6
codered-epidemy <codered-epidemy@github.com>
2023-04-12 20:48:06 +0500

    доделал задание

--- numerical_methods_task4.py
+++ numerical_methods_task4.py
@@ -23,6 +23,11 @@ def formule_3_8(a, b, h):
     return res_3_8
 
 
+def formule_3_8_runge(h):
+    r = (f(h) - f(h / 2)) / 3
+    return abs(r)
+
+
 def formule_trapezoid(a, b, h):
     x_prev = a
     x_curr = x_prev
@@ -37,21 +42,49 @@ def formule_trapezoid(a, b, h):
     return res_trapezoid
 
 
+def formule_trapezoid_runge(h):
+    r = (1 / 3) * (f(h) - f(h / 2))
+    return abs(r)
+
+
+def gauss(a, b, c):
+    return a*f((5-math.sqrt(15))/10)+b*f(1/2)+c*f((5+math.sqrt(15))/10)
+
+
 def main():
     print('Формула 3/8:')
 
     res1 = formule_3_8(0, 1, h1)
     res2 = formule_3_8(0, 1, h2)
     res3 = formule_3_8(0, 1, h3)
-    print(f'Шаг: {h1}. Результат: {res1}', f'Шаг: {h2}. Результат: {res2}', f'Шаг: {h3}. Результат: {res3}', sep='\n')
+    res_runge_3_8_1 = formule_3_8_runge(h1)
+    res_runge_3_8_2 = formule_3_8_runge(h2)
+    res_runge_3_8_3 = formule_3_8_runge(h3)
+    print(f'Шаг: {h1}. Результат: {res1}', f'Шаг: {h2}. Результат: {res2}', f'Шаг: {h3}. Результат: {res3}', sep='\n',
+          end='\n\n')
+    print('Погрешность:', end='\n')
+    print(f'Шаг: {h1}. Результат: {res_runge_3_8_1}', f'Шаг: {h2}. Результат: {res_runge_3_8_2}',
+          f'Шаг: {h3}. Результат: {res_runge_3_8_3}', sep='\n')
+
     print('\n')
+
     res4 = formule_trapezoid(0, 1, h1)
     res5 = formule_trapezoid(0, 1, h2)
     res6 = formule_trapezoid(0, 1, h3)
+    res_runge_trapezoid1 = formule_trapezoid_runge(h1)
+    res_runge_trapezoid2 = formule_trapezoid_runge(h2)
+    res_runge_trapezoid3 = formule_trapezoid_runge(h3)
     print('Формула трапеций:')
+    print(f'Шаг: {h1}. Результат: {res4}', f'Шаг: {h2}. Результат: {res5}', f'Шаг: {h3}. Результат: {res6}', sep='\n',
+          end='\n\n')
+    print('Погрешность:', end='\n')
+    print(f'Шаг: {h1}. Результат: {res_runge_trapezoid1}', f'Шаг: {h2}. Результат: {res_runge_trapezoid2}',
+          f'Шаг: {h3}. Результат: {res_runge_trapezoid3}', sep='\n')
 
-    print(f'Шаг: {h1}. Результат: {res4}', f'Шаг: {h2}. Результат: {res5}', f'Шаг: {h3}. Результат: {res6}', sep='\n')
+    print('\n')
+
+    print(f'Расчет по формуле Гаусса с 3 узлами: {gauss(5/18, 4/9, 5/18)}')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
